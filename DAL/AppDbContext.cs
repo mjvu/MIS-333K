d3fa92151a46d47cf2c40594ad3d20666fe9058c
David O P <davordpal@wireless-10-145-67-49.public.utexas.edu>
2018-11-01 20:52:11 -0500

    AppDbContext Added

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Team32_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Team32_Project.DAL
+{
+      public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    //You need one db set for each model class. For example:
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Coupon> Coupons { get; set; }
+    public DbSet<CreditCard> CreditCards { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<Reorder> Reorders { get; set; }
+    public DbSet<ReorderDetail> ReorderDetails { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<User> Users { get; set; }
+    }
+}
