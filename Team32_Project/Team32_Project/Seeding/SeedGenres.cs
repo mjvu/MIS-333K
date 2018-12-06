449474b32a807a9abeb51bb0e3f7d7dc7cbe0465
David O P <davordpal@Davids-MacBook-Air.local>
2018-12-06 01:02:55 -0600

    Seed books/genres

--- Team32_Project/Team32_Project/Seeding/SeedGenres.cs
+++ Team32_Project/Team32_Project/Seeding/SeedGenres.cs
@@ -11,10 +11,10 @@ namespace Team32_Project.Seeding
         public static void SeedAllGenres(AppDbContext db)
         {
             //check to see if all the genres have already been added
-            if (db.Genres.Count() == 7)
+            if (db.Genres.Count() == 13)
             {
                 //exit the program - we don't need to do any of this
-                NotSupportedException ex = new NotSupportedException("Genre record count is already 7!");
+                NotSupportedException ex = new NotSupportedException("Genre record count is already 13!");
                 throw ex;
             }
             Int32 intGenresAdded = 0;
@@ -23,26 +23,44 @@ namespace Team32_Project.Seeding
                 //Create a list of genres
                 List<Genre> Genres = new List<Genre>();
 
-                Genre g1 = new Genre { GenreName = "Contemporary Fiction" };
+                Genre g1 = new Genre { GenreName = "Adventure" };
                 Genres.Add(g1);
 
-                Genre g2 = new Genre { GenreName = "Science Fiction" };
+                Genre g2 = new Genre { GenreName = "Contemporary Fiction" };
                 Genres.Add(g2);
 
-                Genre g3 = new Genre { GenreName = "Mystery" };
+                Genre g3 = new Genre { GenreName = "Fantasy" };
                 Genres.Add(g3);
 
-                Genre g4 = new Genre { GenreName = "Suspense" };
+                Genre g4 = new Genre { GenreName = "Historical Fiction" };
                 Genres.Add(g4);
 
-                Genre g5 = new Genre { GenreName = "Romance" };
+                Genre g5 = new Genre { GenreName = "Horror" };
                 Genres.Add(g5);
 
-                Genre g6 = new Genre { GenreName = "Thriller" };
+                Genre g6 = new Genre { GenreName = "Humor" };
                 Genres.Add(g6);
 
-                Genre g7 = new Genre { GenreName = "Fantasy" };
+                Genre g7 = new Genre { GenreName = "Mystery" };
                 Genres.Add(g7);
+
+                Genre g8 = new Genre { GenreName = "Poetry" };
+                Genres.Add(g8);
+
+                Genre g9 = new Genre { GenreName = "Romance" };
+                Genres.Add(g9);
+
+                Genre g10 = new Genre { GenreName = "Science Fiction" };
+                Genres.Add(g10);
+
+                Genre g11 = new Genre { GenreName = "Shakespeare" };
+                Genres.Add(g11);
+
+                Genre g12 = new Genre { GenreName = "Suspense" };
+                Genres.Add(g12);
+
+                Genre g13 = new Genre { GenreName = "Thriller" };
+                Genres.Add(g13);
 
                 Genre g;
       
